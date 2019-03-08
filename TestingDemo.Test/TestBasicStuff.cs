f666556759a55641f579f338ded8a8873aa14ff1
Jonas Frid <jonas.frid@avaloninnovation.com>
2019-03-08 08:57:34 +0100

    First commit

---
+++ TestingDemo.Test/TestBasicStuff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestingDemo.Test
+{
+    [TestFixture]
+    public class TestBasicStuff
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            //_employeeRepository = Substitute.For<IEmployeeRepository>();
+        }
+
+        [Test]
+        public void TestNothing()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+            Assert.True(true);
+        }
+    }
+}
+
+
+/*
+_employeeRepository
+    .GetEmployees()
+    .Returns(new List<Employee> { new Employee { Id = 1, FirstName = "Kalle", LastName = "Anka", Department = Departments.Development, MonthlySalary = 10000 } });
+
+_employeeRepository.Received().AddEmployee(Arg.Any<Employee>());
+_employeeRepository.DidNotReceive().AddEmployee(Arg.Any<Employee>());  
+
+[TestCase(1, 1, 20, 20, true)] // Correct
+[TestCase(1, 1, 19, 20, false)] // To few working hours
+public void TestGetAllEmployees(int employeeId, int weekNumber, int numberOfHoursWorked, int numberOfHoursAbsence, bool isValid)
+*/
---
+++ TestingDemo/IoC.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+
+namespace TestingDemo
+{
+    public static class IoC
+    {
+        public static IContainer Container { get; private set; }
+
+        public static void Init()
+        {
+            var builder = new ContainerBuilder();
+
+            //builder.RegisterType<EmployeeService>()?.As<IEmployeeService>()?.SingleInstance();
+
+            Container = builder.Build();
+        }
+    }
+}
